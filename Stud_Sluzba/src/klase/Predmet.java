aa94847d1a08ca382a1ae3338212cf90d212bf7a
vucaj <vucaj1998@outlook.com>
2019-11-07 11:55:10 +0100

    Klase stud i prof

--- Stud_Sluzba/src/klase/Predmet.java
+++ Stud_Sluzba/src/klase/Predmet.java
@@ -1,5 +1,65 @@
 package klase;
 
-public class Predmet {
+import java.util.ArrayList;
 
+public class Predmet {
+	private String mSifraPredmeta;
+	private String mNazivPredmeta;
+	private int mSemestarPredmeta;
+	private int mGodinaIzvodjenja;
+	private Profesor mProfesor;
+	private ArrayList<Student> mListaStudenata; //mozda hashmap bude trebao
+	
+	public Predmet()
+	{
+		this.mListaStudenata = new ArrayList<Student>();
+	}
+	
+	public Predmet(String sifraPredmeta,String nazivPredmeta,int semestarPredmeta,int godinaIzvodjenja,Profesor profesor,ArrayList<Student> listaStudenata)
+	{
+		this.mSifraPredmeta=sifraPredmeta;
+		this.mNazivPredmeta=nazivPredmeta;
+		this.mSemestarPredmeta=semestarPredmeta;
+		this.mGodinaIzvodjenja=godinaIzvodjenja;
+		this.mProfesor=profesor;
+		this.mListaStudenata=listaStudenata; //koji pohadjaju predmet
+	}
+	//verovatno ce trebati u nekom trenutku
+	public String getmSifraPredmeta() {
+		return mSifraPredmeta;
+	}
+	public void setmSifraPredmeta(String mSifraPredmeta) {
+		this.mSifraPredmeta = mSifraPredmeta;
+	}
+	public String getmNazivPredmeta() {
+		return mNazivPredmeta;
+	}
+	public void setmNazivPredmeta(String mNazivPredmeta) {
+		this.mNazivPredmeta = mNazivPredmeta;
+	}
+	public int getmSemestarPredmeta() {
+		return mSemestarPredmeta;
+	}
+	public void setmSemestarPredmeta(int mSemestarPredmeta) {
+		this.mSemestarPredmeta = mSemestarPredmeta;
+	}
+	public int getmGodinaIzvodjenja() {
+		return mGodinaIzvodjenja;
+	}
+	public void setmGodinaIzvodjenja(int mGodinaIzvodjenja) {
+		this.mGodinaIzvodjenja = mGodinaIzvodjenja;
+	}
+	public Profesor getmProfesor() {
+		return mProfesor;
+	}
+	public void setmProfesor(Profesor mProfesor) {
+		this.mProfesor = mProfesor;
+	}
+	public ArrayList<Student> getmListaStudenata() {
+		return mListaStudenata;
+	}
+	public void setmListaStudenata(ArrayList<Student> mListaStudenata) {
+		this.mListaStudenata = mListaStudenata;
+	}	
+	
 }
---
+++ Stud_Sluzba/src/klase/Profesor.java
@@ -0,0 +1,108 @@
+package klase;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Profesor {
+	private String mIme;
+	private String mPrezime;
+	private LocalDate mDatumRodjenja;
+	private String mAdresaStanovanja;
+	private String mTelefon;
+	private String mEmail;
+	private String mAdresaKancelarije;
+	private String mBrojLK; //ima 13 cifara
+	private String mTitula;
+	private String mZvanje;
+	private ArrayList<Predmet> mListaPredmeta; //na kojima predaje
+	
+	public Profesor()
+	{
+		this.mListaPredmeta = new ArrayList<Predmet>();
+	}
+	
+	
+	
+	public Profesor(String mIme, String mPrezime, LocalDate mDatumRodjenja, String mAdresaStanovanja, String mTelefon,String mEmail, String mAdresaKancelarije, String mBrojLK, String mTitula, String mZvanje,ArrayList<Predmet> mListaPredmeta) {
+		super();
+		this.mIme = mIme;
+		this.mPrezime = mPrezime;
+		this.mDatumRodjenja = mDatumRodjenja;
+		this.mAdresaStanovanja = mAdresaStanovanja;
+		this.mTelefon = mTelefon;
+		this.mEmail = mEmail;
+		this.mAdresaKancelarije = mAdresaKancelarije;
+		this.mBrojLK = mBrojLK;
+		this.mTitula = mTitula;
+		this.mZvanje = mZvanje;
+		this.mListaPredmeta = mListaPredmeta;
+	}
+
+	//verovatno ce trebaati kasnije tokom pravljenja
+	public String getmIme() {
+		return mIme;
+	}
+	public void setmIme(String mIme) {
+		this.mIme = mIme;
+	}
+	public String getmPrezime() {
+		return mPrezime;
+	}
+	public void setmPrezime(String mPrezime) {
+		this.mPrezime = mPrezime;
+	}
+	public LocalDate getmDatumRodjenja() {
+		return mDatumRodjenja;
+	}
+	public void setmDatumRodjenja(LocalDate mDatumRodjenja) {
+		this.mDatumRodjenja = mDatumRodjenja;
+	}
+	public String getmAdresaStanovanja() {
+		return mAdresaStanovanja;
+	}
+	public void setmAdresaStanovanja(String mAdresaStanovanja) {
+		this.mAdresaStanovanja = mAdresaStanovanja;
+	}
+	public String getmTelefon() {
+		return mTelefon;
+	}
+	public void setmTelefon(String mTelefon) {
+		this.mTelefon = mTelefon;
+	}
+	public String getmEmail() {
+		return mEmail;
+	}
+	public void setmEmail(String mEmail) {
+		this.mEmail = mEmail;
+	}
+	public String getmAdresaKancelarije() {
+		return mAdresaKancelarije;
+	}
+	public void setmAdresaKancelarije(String mAdresaKancelarije) {
+		this.mAdresaKancelarije = mAdresaKancelarije;
+	}
+	public String getmBrojLK() {
+		return mBrojLK;
+	}
+	public void setmBrojLK(String mBrojLK) {
+		this.mBrojLK = mBrojLK;
+	}
+	public String getmTitula() {
+		return mTitula;
+	}
+	public void setmTitula(String mTitula) {
+		this.mTitula = mTitula;
+	}
+	public String getmZvanje() {
+		return mZvanje;
+	}
+	public void setmZvanje(String mZvanje) {
+		this.mZvanje = mZvanje;
+	}
+	public ArrayList<Predmet> getmListaPredmeta() {
+		return mListaPredmeta;
+	}
+	public void setmListaPredmeta(ArrayList<Predmet> mListaPredmeta) {
+		this.mListaPredmeta = mListaPredmeta;
+	}	
+}
