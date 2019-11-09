1256c8f026d968d28e3849a31ca71a682d7b1305
gregVader <kgerg13@gmail.com>
2019-11-09 16:18:10 +0100

    student controller

--- Stud_Sluzba/src/controller/StudentController.java
+++ Stud_Sluzba/src/controller/StudentController.java
@@ -2,4 +2,29 @@ package controller;
 
 public class StudentController {
 
+	private StudentController instance = null;;
+	
+	private StudentController() {}
+	
+	public StudentController getInstance() {
+		if(instance==null)
+			instance = new StudentController();
+		return instance;
+	}
+	
+	public void dodajStudenta() {
+		//TODO dodaj studenta
+	}
+	
+	public void izmeniStudenta(int row) {
+		//TODO izmena studenta
+	}
+	
+	public void izbrisiStudenta(int row) {
+		//TODO obrisi studenta
+	}
+	
+	public void sort(int column, boolean ascending) {
+		//TODO sortiranje studenta
+	}
 }
