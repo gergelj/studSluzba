737aef6dd2eefe7aa80aa6251a0b6197171b25f8
vucaj <vucaj1998@outlook.com>
2019-11-10 17:26:46 +0100

    Dialog subject, bazaSubj started

--- Stud_Sluzba/src/gui/SubjectPanel.java
+++ Stud_Sluzba/src/gui/SubjectPanel.java
@@ -3,6 +3,7 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.util.Vector;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -18,11 +19,9 @@ public class SubjectPanel extends JPanel {
 	 */
 	private static final long serialVersionUID = -8542726682573525716L;
 	
-	private JTextField sif_pred, naz_pred;
+	private JTextField sif_txt, naz_txt;
 	private JLabel sif_lbl,naz_lbl,sem_lbl,god_lbl;	
-	private JComboBox<String> god_list;
-	private JComboBox<String> sem_list;
-	
+	private JComboBox<String> list;	
 	public SubjectPanel()
 	{
 		setLayout(new BorderLayout());
@@ -35,19 +34,58 @@ public class SubjectPanel extends JPanel {
 		
 		JPanel in_field = new JPanel();
 		in_field.setLayout(new BoxLayout(in_field, BoxLayout.Y_AXIS));
+		
+		in_field.add(add_panel(sif_lbl,sif_txt,"Sifra predmeta *",0));
+		in_field.add(add_panel(naz_lbl,naz_txt,"Naziv predmeta *",0));
+		in_field.add(add_panel(god_lbl,null,"Godina *",1));
+		in_field.add(add_panel(sem_lbl,null,"Semestar *",2));
+		
+		
+		
+		add(in_field,BorderLayout.NORTH);
 	}
 	
-	private JPanel add_panel(JLabel labela,JTextField txt,String ime_labele)
+	
+	private JPanel add_panel(JLabel labela,JTextField txt,String ime_labele,int broj)
 	{
 		JPanel pomocni = new JPanel();
 		Dimension lbl_dimension = new Dimension(150,20);
 		Dimension txt_dimension = new Dimension(200,20);
 		labela = new JLabel(ime_labele);
 		labela.setPreferredSize(lbl_dimension);
-		txt = new JTextField();
-		txt.setPreferredSize(txt_dimension);
 		pomocni.add(labela);
-		pomocni.add(txt);
+		if(broj==0)
+		{
+			txt = new JTextField();
+			txt.setPreferredSize(txt_dimension);
+			pomocni.add(txt);
+		}
+		else
+		{
+			Vector<String> vec = new Vector<String>();
+			if(broj == 1)
+			{
+				vec.add("I (prva)");
+				vec.add("II (druga)");
+				vec.add("III (treća)");
+				vec.add("IV (četvrta)");
+				
+			}
+			else if(broj == 2)
+			{
+				vec.add("I (prvi)");
+				vec.add("II (drugi)");
+				vec.add("III (treći)");
+				vec.add("IV (četvrti)");
+				vec.add("V (peti)");
+				vec.add("VI (sesti)");
+				vec.add("VII (sedmi)");
+				vec.add("VII (osmi)");
+			}
+			list = new JComboBox<String>(vec);
+			list.setPreferredSize(txt_dimension);
+			pomocni.add(list);
+		}
 		return pomocni;
 	}
 }
--- Stud_Sluzba/src/klase/BazaPredmeta.java
+++ Stud_Sluzba/src/klase/BazaPredmeta.java
@@ -1,5 +1,84 @@
 package klase;
 
-public class BazaPredmeta {
+import java.awt.List;
+import java.util.ArrayList;
 
+public class BazaPredmeta {
+	private static BazaPredmeta instance = null;
+	
+	public static BazaPredmeta getInstance()
+	{
+		if(instance == null)
+			instance = new BazaPredmeta();
+		return instance;
+	}
+	
+	private int generator;
+	
+	private ArrayList<Predmet> predmeti;
+	private ArrayList<String>  kolone;
+	
+	private BazaPredmeta()
+	{
+		generator = 0;
+		
+		this.kolone = new ArrayList<String>();
+		this.kolone.add("Sifra predmeta");
+		this.kolone.add("Naziv predmeta");
+		this.kolone.add("Godina");
+		this.kolone.add("Semestar");
+		this.kolone.add("Profesor");
+	}
+	
+	public String getValueAt(int red,int kolona)
+	{
+		Predmet predmet = this.predmeti.get(red);
+		switch (kolona) {
+		case 0:
+			return predmet.getmSifraPredmeta();
+		case 1:
+			return predmet.getmNazivPredmeta();
+		case 2: 
+			return Integer.toString(predmet.getmGodinaIzvodjenja());
+		case 3:
+			return Integer.toString(predmet.getmSemestarPredmeta());
+		/*case 4:
+			return null*/
+		default:
+			return null;
+		}
+	}
+	
+	//public void addPredmet(String sifra, String naziv, Int god, Int semestar)
+	
+	public String getImeKolone(int index)
+	{
+		return this.kolone.get(index);
+	}
+	
+	public Predmet getRed(int index_red)
+	{
+		return this.predmeti.get(index_red);
+	}
+	
+	public ArrayList<Predmet> getPredmeti()
+	{
+		return predmeti;
+	}
+	
+	public void setPredmeti(ArrayList<Predmet> predmeti) 
+	{
+		this.predmeti=predmeti;
+	}
+	
+	private int generateId()
+	{
+		return generator++;
+	}
+	
+	public int getBrojKolona()
+	{
+		return 5;
+	}
+	
 }
