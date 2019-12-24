70dad2ca546923780697ac02f6f2fa6d647f8b9e
Kiss Gergely <49314980+gregVader@users.noreply.github.com>
2019-12-24 20:26:38 +0100

    #statusbar sat prikazuje sekunde

--- Stud_Sluzba/src/gui/DatumThread.java
+++ Stud_Sluzba/src/gui/DatumThread.java
@@ -5,20 +5,21 @@ import java.time.format.DateTimeFormatter;
 
 public class DatumThread extends Thread {
 
-	MainFrame mf;
+	private MainFrame mf;
 	
 	public DatumThread(MainFrame mf) {
 		this.mf = mf;
 	}
-	
+	//private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm   dd.MM.yyyy.");
 	@Override
 	public void run() {
 		while (true) {
 			//osvezava ispis svakog minuta
-			mf.mDateTime.setText(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm   dd.MM.yyyy.")));
+			mf.mDateTime.setText(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss   dd.MM.yyyy.")));
+			
 			try {
 				Thread.currentThread();
-				Thread.sleep(60000);
+				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
